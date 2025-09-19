b6a15539001e17e2550cf93824f70bfba6af506b
eancheta <cireancheta2003@gmail.com>
2025-09-19 19:12:38 +0800

    wishh ko sana maa deploy nato

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,16 +1,19 @@
 import '../css/app.css';
+import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
+import type { DefineComponent } from 'vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    resolve: (name): Promise<DefineComponent> =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+        ) as Promise<DefineComponent>, // <-- cast here
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
@@ -21,5 +24,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-initializeTheme();
