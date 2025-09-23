e65b769d57032305abf92791035fc9c1d7798c6b
eancheta <cireancheta2003@gmail.com>
2025-09-23 21:46:55 +0800

    dasa

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -2,27 +2,20 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cloudinary Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Make sure to set these in your .env file. The package will use these
-    | credentials to upload your images to Cloudinary.
-    |
-    */
-
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
         'api_secret' => env('CLOUDINARY_API_SECRET'),
-        'secure'     => true,
     ],
 
     'url' => [
         'secure' => true,
     ],
 
-    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
-    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+    'upload' => [
+        'preset'           => env('CLOUDINARY_UPLOAD_PRESET', null),
+        'folder'           => env('CLOUDINARY_UPLOAD_FOLDER', null),
+        'notification_url' => env('CLOUDINARY_NOTIFICATION_URL', null),
+    ],
+
 ];
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,19 @@ use App\Http\Controllers\Owner\WalkinController;
 use App\Http\Controllers\Customer\FeedbackController;
 use App\Http\Controllers\Owner\ReviewController;
 
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
+
+Route::get('/test-cloudinary', function() {
+    $filePath = public_path('apple-touch-icon.png'); // your test image
+
+    try {
+        $result = Cloudinary::upload($filePath, ['folder' => 'test_uploads']);
+        return $result->getSecurePath();
+    } catch (\Throwable $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
 
 /*
 |--------------------------------------------------------------------------
