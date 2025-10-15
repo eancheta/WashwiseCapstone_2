088fdfcf62c9468691a761d9c04b3bd629288171
eancheta <cireancheta2003@gmail.com>
2025-10-15 23:44:25 +0800

    fix

--- app/Http/Controllers/OwnerShopController.php
+++ app/Http/Controllers/OwnerShopController.php
@@ -79,7 +79,7 @@ public function update(Request $request)
 
     $shop->update($validated);
 
-    return redirect()->route('carwashownerdashboard')
+    return redirect()->route('owner.shop.edit')
     ->with([
         'flash' => [
             'success' => 'Shop details updated successfully.'
